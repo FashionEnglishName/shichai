4aa3b7d9ce558334f7b84a3696168a93173dd6ad
DongMinghui <995862101@qq.com>
2018-01-26 08:12:25 +0000

    inport rubick.css

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>@yield("title","shichai")</title>
+    <link rel="stylesheet" href="/css/app.css">
+
+</head>
+<body>
+    @yield("contents")
+</body>
+</html>
