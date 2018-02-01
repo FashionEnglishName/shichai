2bce6896aa09f72a19fc86d7711a5096c2da6311
DongMinghui <995862101@qq.com>
2018-02-01 03:48:17 +0000

    inport pres_rubick.css and js

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,7 +3,9 @@
 <head>
     <title>@yield("title","shichai")</title>
     <link rel="stylesheet" href="/css/app.css">
-
+    {{--<link rel="stylesheet" href="/css/rubick.css">--}}
+    <script language="JavaScript" src="/js/app.js"></script>
+    {{--<script language="JavaScript" src="/js/rubick.js"></script>--}}
 </head>
 <body>
     @yield("contents")
--- resources/views/main-pages/home.blade.php
+++ resources/views/main-pages/home.blade.php
@@ -2,5 +2,62 @@
 @section("title","home")
 
 @section("contents")
-
+    <nav class="navbar navbar-default navbar-transparent navbar-fixed-top navbar-burger" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button id="menu-toggle" type="button" class="navbar-toggle" data-toggle="collapse" data-target="#example">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar bar1"></span>
+                    <span class="icon-bar bar2"></span>
+                    <span class="icon-bar bar3"></span>
+                </button>
+                <a href="#" class="navbar-brand">拾柴</a>
+            </div>
+            <div class="collapse navbar-collapse" >
+                <ul class="nav navbar-nav navbar-right navbar-uppercase">
+                    <li>
+                        <a href="">
+                            About
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            What We Do
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            Famous Freebies
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            Premium UI Tools
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            Testimonials
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            Team
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            Numbers
+                        </a>
+                    </li>
+                    <li>
+                        <a href="" >
+                            Contact
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+    </nav>
 @stop
