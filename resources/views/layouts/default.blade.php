b8cb7b0f77bd9299fcfbf48864635a829223ff94
DongMinghui <995862101@qq.com>
2018-07-10 02:14:26 +0000

    change relative address to saset function

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,9 +4,9 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield("title","shichai")</title>
-    <link rel="stylesheet" href="/css/app.css">
-    <link rel="stylesheet" href="/css/toastr.css">
-    <link rel="stylesheet" href="/css/cropper.css">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/toastr.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/cropper.css') }}">
     @yield("style")
     <script language="JavaScript" src="/js/app.js"></script>
     <script language="JavaScript" src="/js/toastr.js"></script>
